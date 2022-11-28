f10ae8348122a8aa542dc48b0674906c01a6f2c8
zarrin-tasnim <zarrintasnim200420@gmail.com>
2022-11-28 12:07:02 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jvipmvj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
 app.get('/', async (req, res) => {
     res.send('The BOOK-Seller portal server is running');
 })
